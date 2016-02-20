2fc4338d04315675e42f9689f9ac4e19d534861f
Justin Woo <moomoowoo@gmail.com>
2016-02-20 10:05:32 +0900

    make search observable and dispose for new queries before resolution
    when new queries come in before the old query has resolved, the search
    using the old query should be cancelled.

--- cyclejs-solution/drivers/fetch.js
+++ cyclejs-solution/drivers/fetch.js
@@ -1,15 +1,8 @@
-import { ReplaySubject } from 'rx';
-
 import search from '../api';
 
 export default function fetchDriver(query$) {
-  const friends$ = new ReplaySubject(1);
-
-  query$.subscribe(query => {
-    search(query, friends => {
-      friends$.onNext(friends);
-    });
-  });
-
-  return friends$
+  // for each query item, create a new observable to pass downstream
+  // our search observable will be active until the next query comes in,
+  // when we will unsubscribe and dispose of the previous search observable
+  return query$.flatMapLatest(x => search(x));
 }
