ce8bf28bacce819b9de1bf8ce9d72a907953a29e
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-04-01 11:31:10 -0400

    feat(apit): make the api search faster, and return a stream to be truly cancelable

--- motorcyclejs-solution/src/api/index.js
+++ motorcyclejs-solution/src/api/index.js
@@ -1,21 +1,33 @@
+import {create} from 'most'
 import friends from './friends'
 
 // mock api search
-export default function search(query = '', callback) {
+export default function search(query = ``) {
   const results = friends.filter(friend => {
-    let keep = false
-
-    Object.keys(friend).forEach(key => {
-      const val = friend[key].toString()
-
-      if (val.toLowerCase().includes(query.toLowerCase())) {
-        keep = true
+    const keys = Object.keys(friend)
+    // faster search
+    for (let i = 0; i < keys.length; ++i) {
+      const val = friend[keys[i]].toString().toLowerCase()
+      if (val.includes(query.toLowerCase())) {
+        return true
       }
-    })
-
-    return keep
+    }
+    return false
   })
 
-  // setting a more realistic (random) timeout
-  setTimeout(() => callback(results), Math.ceil(Math.random() * 250))
+  // use an stream for our search API so that it's actually cancellable when we dispose of our subscription
+  return create((next) => {
+    const timeout = setTimeout(() => {
+      console.log(`RESOLVING search ${timeout}`)
+      next(results)
+    }, Math.ceil(100 + Math.random() * 500)) // make delay longer to make cancellation and loading screen obvious
+
+    next('loading') // send 'loading' state
+    console.log(`STARTING search ${timeout}`)
+
+    return () => {
+      console.log(`DISPOSING search ${timeout}`)
+      clearTimeout(timeout)
+    }
+  }).startWith(friends)
 }
