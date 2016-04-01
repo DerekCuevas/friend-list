41c89012e500bc4aaeab62e362e65fff793edd52
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-04-01 11:32:12 -0400

    feat(): adjust query driver to work with new api

--- motorcyclejs-solution/src/drivers/fetch.js
+++ motorcyclejs-solution/src/drivers/fetch.js
@@ -1,14 +1,2 @@
-import {of} from 'most'
-import {holdSubject} from 'most-subject'
-
 import search from '../api'
-
-function fetchDriver(query$) {
-  const {observer, stream} = holdSubject()
-
-  query$.observe(query => search(query, friend => observer.next(of(friend))))
-
-  return stream.switch() // cancels previous requests
-}
-
-export default fetchDriver
+export default query$ => query$.map(search).switch()
